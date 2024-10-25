6ce73f45eb282d77a3573dc5d4cb1986b7dcd045
bakraw <161661948+bakraw@users.noreply.github.com>
2024-10-25 11:24:49 +0200

    Update import-data.sql

--- import-data.sql
+++ import-data.sql
@@ -1,9 +1,9 @@
 USE dolibarr;
 
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/data.csv'
+LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/data.csv'
 INTO TABLE llx_societe
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(type, nom, adresse, zip, town, pays, phone, email);
+(nom, code_client, address, zip, town, fk_pays, phone, email, client, fournisseur);
